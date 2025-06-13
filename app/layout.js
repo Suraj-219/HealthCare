345a7697ba627996088af7bf9252235df1a9c6b7
Suman Kar <www.skar2022@gmail.com>
2025-06-13 21:55:07 +0530

    icon change

--- app/layout.js
+++ app/layout.js
@@ -23,8 +23,7 @@ export default function RootLayout({ children }) {
       <html lang="en" suppressHydrationWarning>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-          <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+          <link rel="icon" href="/android-chrome-192x192.png" type="image/png" />
         </head>
         <body
           className={`${inter.className}`}
