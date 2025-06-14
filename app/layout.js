1e7c412982beddcb5e79e447fd9454971619cf75
Suman Kar <www.skar2022@gmail.com>
2025-06-14 14:26:08 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -61,7 +61,7 @@ export default function RootLayout({ children }) {
       <html lang="en" suppressHydrationWarning>
         <head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/logo.png" type="image/png" />
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         </head>
         <body
           className={`${inter.className}`}
